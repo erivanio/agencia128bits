4a9a10bcccf9d3d57ff36983ce7190394e0bf2cd
Erivanio <erivanio.vanin@gmail.com>
2013-08-20 20:33:02 -0300

    Organizando o diretório dos arquivos estáticos.

--- agencia128bits/settings.py
+++ agencia128bits/settings.py
@@ -1,6 +1,5 @@
 # Django settings for agencia128bits project.
 import os
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -50,7 +49,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '..', 'media').replace('\\','/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
